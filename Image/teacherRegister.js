e69356e547cbed96af8154fc0ed7dee6cd7c2359
irinnisha <71202635+irinnisha@users.noreply.github.com>
2023-11-18 21:09:25 +0600

    qqw

---
+++ Image/teacherRegister.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('multiStepForm');
+    const formSteps = document.querySelectorAll('.form-step');
+    const progressBar = document.querySelector('.progress-bar');
+
+    form.addEventListener('click', function (e) {
+        if (e.target.classList.contains('next-btn') || e.target.classList.contains('prev-btn')) {
+            e.preventDefault();
+            const currentStep = e.target.closest('.form-step');
+            const nextStep = e.target.dataset.step;
+
+            updateProgressBar(nextStep);
+
+            currentStep.style.display = 'none';
+            formSteps[nextStep - 1].style.display = 'block';
+        }
+    });
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        alert('Form submitted successfully!');
+    });
+
+    function updateProgressBar(step) {
+        const steps = Array.from(document.querySelectorAll('.step'));
+
+        steps.forEach((stepElem) => {
+            const stepNumber = stepElem.dataset.step;
+            stepElem.classList.remove('completed');
+            if (stepNumber <= step) {
+                stepElem.classList.add('completed');
+            }
+        });
+    }
+});
