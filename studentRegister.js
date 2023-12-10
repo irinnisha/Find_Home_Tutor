573dd9a7ad6e07c114ea856836ec6b612f556247
rootkityasin <107543212+rootkityasin@users.noreply.github.com>
2023-12-10 14:25:58 +0600

    Revert "Merge branch 'main' of https://github.com/irinnisha/Find_Home_Tutor"
    This reverts commit 9c5f4965e38c67992a7c0db3d4e55994d4d1909b, reversing
    changes made to a337e05f825d5890be93557225693d20f41370f1.

--- studentRegister.js
+++ studentRegister.js
@@ -6,49 +6,19 @@ document.addEventListener('DOMContentLoaded', function () {
     form.addEventListener('click', function (e) {
         if (e.target.classList.contains('next-btn') || e.target.classList.contains('prev-btn')) {
             e.preventDefault();
-            if (validateCurrentStep()) {
-                const currentStep = e.target.closest('.form-step');
-                const nextStep = e.target.dataset.step;
+            const currentStep = e.target.closest('.form-step');
+            const nextStep = e.target.dataset.step;
 
-                updateProgressBar(nextStep);
+            updateProgressBar(nextStep);
 
-                currentStep.style.display = 'none';
-                formSteps[nextStep - 1].style.display = 'block';
-            }
+            currentStep.style.display = 'none';
+            formSteps[nextStep - 1].style.display = 'block';
         }
     });
 
     form.addEventListener('submit', function (e) {
         e.preventDefault();
-        if (validateForm()) {
-            // Get form data
-            const formData = new FormData(form);
-
-            // Make an AJAX request
-            const xhr = new XMLHttpRequest();
-            xhr.open('POST', 'process_registration.php', true);
-            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-            xhr.onload = function () {
-                if (xhr.status >= 200 && xhr.status < 400) {
-                    // Success
-                    console.log(xhr.responseText);
-
-                    // Optionally, you can redirect or perform other actions based on the response
-                    // window.location.href = 'success_page.html';
-                } else {
-                    // Error
-                    console.error('Request failed with status:', xhr.status);
-                }
-            };
-
-            xhr.onerror = function () {
-                console.error('Request failed');
-            };
-
-            // Send the form data
-            xhr.send(new URLSearchParams(formData));
-        }
+        alert('Form submitted successfully!');
     });
 
     function updateProgressBar(step) {
@@ -61,37 +31,5 @@ document.addEventListener('DOMContentLoaded', function () {
                 stepElem.classList.add('completed');
             }
         });
-    }
-
-    function validateCurrentStep() {
-        const currentStep = form.querySelector('.form-step:not([style="display: none;"])');
-        const requiredFields = currentStep.querySelectorAll('input[required], select[required]');
-
-        for (const field of requiredFields) {
-            if (!field.value.trim()) {
-                alert('Please fill out all required fields.');
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    function validateForm() {
-        const allSteps = Array.from(formSteps);
-        const requiredFields = allSteps.reduce((fields, step) => {
-            const stepFields = step.querySelectorAll('input[required], select[required]');
-            fields.push(...stepFields);
-            return fields;
-        }, []);
-
-        for (const field of requiredFields) {
-            if (!field.value.trim()) {
-                alert('Please fill out all required fields.');
-                return false;
-            }
-        }
-
-        return true;
     }
 });
